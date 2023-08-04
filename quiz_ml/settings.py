63899548e1b42acea0f7ed2ed193d5e036a69ea9
Mouhamadou Lamine THIAM <ply_thiam@hotmail.com>
2023-08-04 16:32:04 +0000

    bakeli dashboard test1

--- quiz_ml/settings.py
+++ quiz_ml/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-7#@+y@@m079kac+hm@((0c22a=8(y=8c95)(qw1$m$by$7ha+#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['btraining.onrender.com']
 
 AUTH_USER_MODEL = "api_quizz.User"
 
